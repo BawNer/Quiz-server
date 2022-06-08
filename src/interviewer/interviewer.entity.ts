09e983ab0893fad95de18cc43d9ffa1a5ea48607
BawNer <aleksandr.krasnikov2000@gmail.com>
2022-06-08 17:32:53 +0300

    add interviewer entity

---
+++ src/interviewer/interviewer.entity.ts
@@ -0,0 +1,42 @@
+import { BeforeInsert, Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity('interviewers')
+export class InterviewerEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  bio: string;
+
+  @Column()
+  email: string;
+
+  @Column({ default: false })
+  isEmailActive: boolean;
+
+  @Column({ default: null })
+  activationCode: string;
+
+  @Column({ default: null })
+  phone: string;
+
+  @Column({ type: 'simple-json', default: null })
+  results: {
+    title: string;
+    questions: {
+      label: string;
+      isAnswerRight: boolean;
+      userAnswerThis: boolean;
+    }[];
+    isAnswerRight: boolean;
+  }[];
+
+  @Column({ default: '' })
+  link: string;
+
+  @BeforeInsert()
+  async generateLink() {
+    this.link = await uuid();
+  }
+}
