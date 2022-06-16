5d80deb22de96141682090f012da972bf5f96c3b
BawNer <aleksandr.krasnikov2000@gmail.com>
2022-06-16 17:16:22 +0300

    update migration query

--- src/migrations/1655384367335-ll.ts
+++ src/migrations/1655384367335-ll.ts
@@ -1,7 +1,7 @@
 import {MigrationInterface, QueryRunner} from "typeorm";
 
-export class optima1654863686126 implements MigrationInterface {
-    name = 'optima1654863686126'
+export class ll1655384367335 implements MigrationInterface {
+    name = 'll1655384367335'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`CREATE TABLE "users" ("id" SERIAL NOT NULL, "email" character varying NOT NULL, "bio" character varying NOT NULL DEFAULT '', "password" character varying NOT NULL, CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`);
@@ -10,7 +10,7 @@ export class optima1654863686126 implements MigrationInterface {
         await queryRunner.query(`CREATE TABLE "interviewers" ("id" SERIAL NOT NULL, "bio" character varying NOT NULL, "email" character varying NOT NULL, "isEmailActive" boolean NOT NULL DEFAULT false, "activationCode" character varying, "phone" character varying, "results" text, "link" character varying NOT NULL DEFAULT '', "quizId" integer, CONSTRAINT "PK_f7a7314e1e7d88e6751ea54a349" PRIMARY KEY ("id"))`);
         await queryRunner.query(`ALTER TABLE "quiz" ADD CONSTRAINT "FK_82f71b10f6749c8e9d3e835668b" FOREIGN KEY ("authorId") REFERENCES "users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
         await queryRunner.query(`ALTER TABLE "quiz" ADD CONSTRAINT "FK_98660b5f774b342a97a407c1097" FOREIGN KEY ("positionId") REFERENCES "positions"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
-        await queryRunner.query(`ALTER TABLE "interviewers" ADD CONSTRAINT "FK_3b2dd07769c5ffe3bf76043ee30" FOREIGN KEY ("quizId") REFERENCES "quiz"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(`ALTER TABLE "interviewers" ADD CONSTRAINT "FK_3b2dd07769c5ffe3bf76043ee30" FOREIGN KEY ("quizId") REFERENCES "quiz"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
